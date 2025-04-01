0f1a011fc3b16e3fcc602558c210563bbfb52b74
nabilmuafa <nmuafa@gmail.com>
2025-04-01 18:39:43 +0700

    add collectible stick

---
+++ scenes/Collectible.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var player_in_range = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("interact") and player_in_range:
+		queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		player_in_range = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		player_in_range = false
