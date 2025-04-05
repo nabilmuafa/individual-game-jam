61c93fb6573928a8f9ccc7a7e2fa043f75ff3ffd
nabilmuafa <nmuafa@gmail.com>
2025-04-06 00:12:12 +0700

    add more collectible items

--- scripts/Collectible.gd
+++ scripts/Collectible.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var player_in_range = false
 @export var item_name: String
+@export var unlimited = false
+@export var max_take = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,9 +12,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_pressed("interact") and player_in_range:
+	if Input.is_action_just_pressed("interact") and player_in_range:
 		PlayerInventory.add_item(item_name, 1)
-		queue_free()
+		if not unlimited:
+			max_take -= 1
+			if max_take == 0:
+				queue_free()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
